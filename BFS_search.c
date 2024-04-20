339c763a098e62e70f41f0aa245ddee66ea98136
sajjad yami <sajjadyami77@gmail.com>
2024-04-20 14:03:48 +0330

    init_visited

--- BFS_search.c
+++ BFS_search.c
@@ -3,8 +3,12 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-
+bool init_visited(graph *g, bool *visited_list){
+    visited_list = calloc(g->num_nodes, sizeof (bool));
+    return visited_list;
+}
 bool do_BFS_Search(graph *g, int from, int to, bool *visited){
+    init_visited(g, visited);
     bool *connection = g->edges[from];
     bool found = false;
     for (int i = 0; i < g->num_nodes; ++i) {
